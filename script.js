c9cedf5b738f20f705610601808864462012ec32
William Long <willhlong@gmail.com>
2023-02-24 12:27:26 -0500

    Added remove book function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ form.addEventListener("submit", addBookToLibrary);
 addBookButton.addEventListener("click", toggleOpenClose);
 cancelEntryButton.addEventListener("click", toggleOpenClose);
 
+
 let myLibrary = [];
 
 function Book(title, author, numPages, read, ...description) {
@@ -22,7 +23,6 @@ function Book(title, author, numPages, read, ...description) {
 Book.prototype.printBook = function () {
   let book = document.createElement("article");
   book.setAttribute("class", "book");
-  book.setAttribute("id", this.bookNumber);
   book.innerHTML = 
     `<div class=\"book-flip-inner\">
       <div class=\"book-flip-front\">
@@ -31,8 +31,8 @@ Book.prototype.printBook = function () {
       </div>
       <div class=\"book-flip-back\">
         <p>${this.description}</p>
-        <p>Page count: ${this.numPages}</p?
-        <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\">
+        <p>Page count: ${this.numPages}</p>
+        <svg class="trash-button" onclick="deleteBook(${this.bookNumber})" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\">
           <title>trash-can-outline</title>
           <path d=\"M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z\" />
         </svg>  
@@ -55,6 +55,7 @@ function addBookToLibrary(event) {
     book.bookNumber = myLibrary.length;
     myLibrary.push(book);
   }
+  toggleOpenClose();
   displayLibrary();
 }
 
@@ -70,6 +71,7 @@ function toggleOpenClose() {
 }
 
 function displayLibrary() {
+  document.querySelector(".book-grid").innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
     myLibrary[i].printBook();
   }
@@ -84,4 +86,9 @@ function displayLibrary() {
       }
     });
   });
+}
+
+function deleteBook(bookNumber) {
+  myLibrary.splice(bookNumber, 1);
+  displayLibrary();
 }
